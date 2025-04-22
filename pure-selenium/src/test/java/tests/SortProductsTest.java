e3d829e533049ccb332857710df626311412b226
Blazej Kanczkowski <blazejkanczkowski@Blazej-Kanczkowski.local>
2025-04-22 14:19:00 +0200

    refactoring

--- pure-selenium/src/test/java/tests/SortProductsTest.java
+++ pure-selenium/src/test/java/tests/SortProductsTest.java
@@ -13,7 +13,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.Collections;
 import java.util.List;
 
-public class SortProductsTest extends AbstractTest{
+public class SortProductsTest extends AbstractTest {
 
     @Test
     public void testSortByPriceHighToLow() {
